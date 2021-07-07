acc2c8f47cb4a9b5b0991b5d74351d0fa739eec3
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-07-06 20:04:01 -0500

    practice

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.\n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.print("Enter a number please: ");
+//        int userInput = scanner.nextInt();
+//
+//        System.out.println("You entered: \"" + userInput + "\"");
+
+//        System.out.print("Please enter three words: ");
+//        String userInput = scanner.next();
+//        String userInput1 = scanner.next();
+//        String userInput2 = scanner.next();
+//        scanner.next();
+//        String userInput4 = scanner.next();
+//
+//        System.out.printf("Your three words are: %s, %s, %s, %s", userInput, userInput1, userInput2, userInput4);
+        /*scanner.useDelimiter("\n");*/
+        System.out.print("Please enter a sentence: ");
+        String userInput = scanner.next();
+        System.out.printf("Sentence is: %s", userInput);
+
+
+//        System.out.println("Please enter a length: ");
+//        double length = parseDouble(scanner.nextLine());
+//        System.out.println("Please enter a width: ");
+//        double width = parseDouble(scanner.nextLine());
+//        double area = length * width;
+//        double perimeter = (length * 2) + (width * 2);
+//        System.out.printf("The area of the room is : %s. The perimeter of the room is : %s.", area, perimeter);
+
+
+
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,20 +1,26 @@
 public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
-//
-//        int myFavoriteNumber = 3;
-//        System.out.println(myFavoriteNumber);
-//
-//        String myString = "I am a string";
-//        System.out.println(myString);
-//
-//        float myNumber = (float) 3.14;
-//        System.out.println(myNumber);
-//
-////        int x = 5;
-////        System.out.println(x++);
-////        System.out.println(x);
-//
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I am a string";
+        System.out.println(myString);
+
+//        myString = 'v';
+
+//        myString = 3.14159;
+
+//        long myNums = 0;
+//        System.out.println(myNums);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
 //        int x = 5;
 //        System.out.println(++x);
 //        System.out.println(x);
@@ -23,16 +29,25 @@ public class HelloWorld {
 //        Object o = theNumberThree;
 //        int three = (int) o;
 
-        int x = 4;
-        x = x + 5;
-
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+//
 //        int x = 3;
 //        int y = 4;
 //        y = y * x;
+//        y *= x;
 //
 //        int x = 10;
 //        int y = 2;
 //        x = x / y;
 //        y = y - x;
+//        x /= y;
+//        y -= x;
+
+        short q = (short) Integer.MAX_VALUE;
+        System.out.printf("%d", q);
     }
+
+
 }
